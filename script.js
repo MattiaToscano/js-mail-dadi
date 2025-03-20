29bdf7793966c458a9908f5b6e7fa841ebfeaf27
MattiaToscano <mattia1997ct@gmail.com>
2025-03-20 13:43:46 +0100

    [UPD] Fine esercizio 1

--- script.js
+++ script.js
@@ -7,4 +7,12 @@ const listaMail = ['mattia@live.it' , 'turi@live.it' , 'melo@live.it' , 'alfio@l
 //Chiedo all'utente di inserire la sua mail.
 let userMail =(prompt('Inserisci la tua mail'));
 
-
+//Inizializzo ciclo per controllo mail.
+for (let i=0; i < listaMail.length; i++) {
+    if (userMail == listaMail[i]) {
+        console.log('La tua mail è presente nella lista, benvenuto/a.')
+    }
+else{
+    console.log('La tua mail non è presente nella lista, ci spiace.')
+}
+}
