6a77fce177ca2a1d5d9dffb4eedbc8d498d870e1
MattiaToscano <mattia1997ct@gmail.com>
2025-03-20 13:37:46 +0100

    [UPD] Inserito prompt richiesta mail

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 
 
 //inizializzo array con lista mail.
-const listaMail ['mattia@live.it' ; 'turi@live.it' ; 'melo@live.it' ; 'alfio@live.it' ]
+const listaMail = ['mattia@live.it' , 'turi@live.it' , 'melo@live.it' , 'alfio@live.it' ];
+
+
+//Chiedo all'utente di inserire la sua mail.
+let userMail =(prompt('Inserisci la tua mail'));
+
+
